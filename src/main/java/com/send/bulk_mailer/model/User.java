d867fb4d95413a372abc6164fe224bc7f5b6ecc8
Amit Mahatha <mahathaamitk@gmail.com>
2026-06-16 21:20:36 +0530

    Create User model and configure OpenCSV dependency

---
+++ src/main/java/com/send/bulk_mailer/model/User.java
@@ -0,0 +1,15 @@
+package com.send.bulk_mailer.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private String name;
+
+    private String email;
+
+    private String brandName;
+
+    private String sendMail;
+}
