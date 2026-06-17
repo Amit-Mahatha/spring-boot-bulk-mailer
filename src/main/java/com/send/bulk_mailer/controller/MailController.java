2fe9260af1650a519565460d0ef9d0acc0b22139
Amit Mahatha <mahathaamitk@gmail.com>
2026-06-17 13:14:05 +0530

    Implemented CSV reading and user mapping

--- src/main/java/com/send/bulk_mailer/controller/MailController.java
+++ src/main/java/com/send/bulk_mailer/controller/MailController.java
@@ -1,21 +1,33 @@
 package com.send.bulk_mailer.controller;
 
+import com.send.bulk_mailer.model.User;
+import com.send.bulk_mailer.service.CsvService;
 import com.send.bulk_mailer.service.MailService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class MailController {
 
     private final MailService mailService;
+    private final CsvService csvService;
 
-    public MailController(MailService mailService) {
+    public MailController(MailService mailService,
+                          CsvService csvService) {
+
         this.mailService = mailService;
+        this.csvService = csvService;
     }
 
     @GetMapping("/hello")
     public String hello() {
-
         return mailService.getMessage();
+    }
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return csvService.readUsers();
     }
 }
---
+++ src/main/java/com/send/bulk_mailer/service/CsvService.java
@@ -0,0 +1,57 @@
+package com.send.bulk_mailer.service;
+
+import com.opencsv.CSVReader;
+import com.send.bulk_mailer.model.User;
+import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CsvService {
+
+    public List<User> readUsers() {
+
+        List<User> users = new ArrayList<>();
+
+        try {
+
+            InputStream inputStream =
+                    getClass()
+                            .getClassLoader()
+                            .getResourceAsStream("users.csv");
+
+            if (inputStream == null) {
+                throw new RuntimeException("users.csv file not found");
+            }
+
+            CSVReader csvReader =
+                    new CSVReader(
+                            new InputStreamReader(inputStream)
+                    );
+
+            List<String[]> rows = csvReader.readAll();
+
+            for (int i = 1; i < rows.size(); i++) {
+
+                String[] row = rows.get(i);
+
+                User user = new User();
+
+                user.setName(row[0]);
+                user.setEmail(row[1]);
+                user.setBrandName(row[2]);
+                user.setSendMail(row[3]);
+
+                users.add(user);
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException("Error reading CSV file", e);
+        }
+
+        return users;
+    }
+}
